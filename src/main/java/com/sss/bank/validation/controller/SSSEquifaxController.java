982597b5729b524d0e3c192057536a93d0a0819b
vinayv1979 <vinay.vijayakumar79@gmail.com>
2019-07-18 09:37:34 +0100

    Exception handling

--- src/main/java/com/sss/bank/validation/controller/SSSEquifaxController.java
+++ src/main/java/com/sss/bank/validation/controller/SSSEquifaxController.java
@@ -3,7 +3,8 @@ package com.sss.bank.validation.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sss.bank.validation.api.pojo.BankResponse;
+import com.sss.bank.validation.api.pojo.ErrorResponse;
+import com.sss.bank.validation.exception.ModCheckNotFoundException;
+import com.sss.bank.validation.exception.ModCheckValidationException;
 import com.sss.bank.validation.service.BankValidationSrvIntf;
 
 @RestController
@@ -25,8 +29,24 @@ public class SSSEquifaxController {
 			@RequestParam(required = true, value = "account") String account) {
 
 		BankResponse bankResponse = null;
+		Boolean valid = false;
 
-		Boolean valid = bankValidationSrvIntf.validate(sortcode, account);
+		String sc = sortcode.replaceAll("\\s", "");
+		String ac = account.replaceAll("\\s", "");
+
+		if (StringUtils.hasText(sc) && sc.length() != 0) {
+
+			if (StringUtils.hasText(ac) && ac.length() != 0) {
+
+				valid = bankValidationSrvIntf.validate(sortcode, account);
+
+			} else {
+				throw new ModCheckValidationException("Input not correct or null");
+
+			}
+			throw new ModCheckValidationException("Input not correct or null");
+
+		}
 
 		if (valid.equals(true)) {
 
@@ -34,13 +54,29 @@ public class SSSEquifaxController {
 			bankResponse.setValid("Account is valid");
 
 		} else {
-			
+
 			bankResponse = new BankResponse();
 			bankResponse.setValid("Account is not valid");
 		}
 
 		return new ResponseEntity<Object>(bankResponse, HttpStatus.OK);
 
+	}
+
+	@ExceptionHandler(ModCheckValidationException.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex) {
+		ErrorResponse error = new ErrorResponse();
+		error.setErrorCode(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(ex.getMessage());
+		return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
+	}
+
+	@ExceptionHandler(ModCheckNotFoundException.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler1(Exception ex) {
+		ErrorResponse error = new ErrorResponse();
+		error.setErrorCode(HttpStatus.NOT_FOUND.value());
+		error.setMessage(ex.getMessage());
+		return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
 	}
 
 }
